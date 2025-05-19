b5a9eb34bc1a370c26bb775a9e556f67bdbccac7
Annika Damstedt <acdamstedt@ucsb.edu>
2025-05-19 14:48:49 -0700

    ad - base set up for admin backend

--- src/test/java/edu/ucsb/cs156/frontiers/controllers/AdminsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/controllers/AdminsControllerTests.java
@@ -1,5 +1,46 @@
 package edu.ucsb.cs156.frontiers.controllers;
 
-public class AdminsControllerTests {
-    
+import edu.ucsb.cs156.frontiers.repositories.UserRepository;
+import edu.ucsb.cs156.frontiers.testconfig.TestConfig;
+import edu.ucsb.cs156.frontiers.ControllerTestCase;
+import edu.ucsb.cs156.frontiers.entities.Admin;
+import edu.ucsb.cs156.frontiers.repositories.AdminRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import java.time.LocalDateTime;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = AdminsController.class)
+@Import(TestConfig.class)
+public class AdminsControllerTests extends ControllerTestCase {
+
+        @MockBean
+        AdminRepository adminRepository;
+
+        @MockBean
+        UserRepository userRepository;
+
 }
+
